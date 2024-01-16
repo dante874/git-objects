f1f0f7a53768e8f09cc5b4f4b56f59da31841f3a
dante <dantedros333@gmail.com>
2024-01-16 11:07:54 +0000

    objects

---
+++ app.js
@@ -0,0 +1,23 @@
+const person = {
+    name: "Bob",
+    age: 30,
+    favouriteColor: "red",
+
+};
+
+console.log(person);
+console.table(person);
+console.log(person.name);
+
+
+
+const car = {
+    make: "nissan",
+    model: 234833,
+    color: "black",
+
+}
+
+console.log(car);
+
+
